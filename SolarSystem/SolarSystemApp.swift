4eaa53bb49bf95181781e5532e022f25bf2ec029
Kevin Massol <kevin.massol@icloud.com>
2023-05-30 16:28:02 +0200

    Initial Commit

---
+++ SolarSystem/SolarSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSystemApp.swift
+//  SolarSystem
+//
+//  Created by KEVIN on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SolarSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
